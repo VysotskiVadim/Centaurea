2a9297801ad56d0d65bebaf093dfaf025886e5b3
Vadzim Vysotski <vadzimv@playtika.com>
2018-03-25 12:45:09 +0300

    it's alive

--- src/droid/app/src/main/java/centaureaproj/com/centaurea/RendererWrapper.java
+++ src/droid/app/src/main/java/centaureaproj/com/centaurea/RendererWrapper.java
@@ -13,16 +13,16 @@ public class RendererWrapper implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 gl10, javax.microedition.khronos.egl.EGLConfig eglConfig) {
-        glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+        GameLibJNIWrapper.on_surface_created();
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-        // No-op
+        GameLibJNIWrapper.on_surface_changed(width, height);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        glClear(GL_COLOR_BUFFER_BIT);
+        GameLibJNIWrapper.on_draw_frame();
     }
 }
