40ff73ff1385e2e0a5849da62765ee109d723369
Vadzim Vysotski <vadzimv@playtika.com>
2018-04-19 19:41:53 +0300

    code cleanup for ios texture loader

--- src/ios/centaurea/IosTextureLoader.cpp
+++ src/ios/centaurea/IosTextureLoader.cpp
@@ -5,26 +5,15 @@
 
 void IosTextureLoader::loadTexture(std::string id) {
     std::string texturePath = "texture_" + id + ".png";
-    UIImage* image = [UIImage imageNamed:[NSString stringWithCString:texturePath.c_str() encoding:[NSString defaultCStringEncoding]]];
-    CGImageRef imageRef = [image CGImage];
-    int width = CGImageGetWidth(imageRef);
-    int height = CGImageGetHeight(imageRef);
+    UIImage* image = [UIImage imageNamed:[NSString stringWithCString:texturePath.c_str()
+                                                            encoding:[NSString defaultCStringEncoding]]];
+    GLubyte* imageData = (GLubyte *)malloc(image.size.width * image.size.height * 4);
+    CGContextRef imageContext = CGBitmapContextCreate(imageData, image.size.width, image.size.height, 8, image.size.width * 4, CGColorSpaceCreateDeviceRGB(), kCGImageAlphaPremultipliedLast);
+    CGContextDrawImage(imageContext, CGRectMake(0.0, 0.0, image.size.width, image.size.height), image.CGImage);
+    CGContextRelease(imageContext);
     
-    GLubyte* textureData = (GLubyte *)malloc(width * height * 4); // if 4 components per pixel (RGBA)
-    
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    NSUInteger bytesPerPixel = 4;
-    NSUInteger bytesPerRow = bytesPerPixel * width;
-    NSUInteger bitsPerComponent = 8;
-    CGContextRef context = CGBitmapContextCreate(textureData, width, height,
-                                                 bitsPerComponent, bytesPerRow, colorSpace,
-                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
-    
-    CGColorSpaceRelease(colorSpace);
-    
-    CGContextDrawImage(context, CGRectMake(0, 0, width, height), imageRef);
-    CGContextRelease(context);
-    
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.size.width, image.size.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    free(imageData);
 }
